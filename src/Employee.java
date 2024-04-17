e87c23a5e802edf42a685c1088a8eaf75c3a7ea2
RossNeyman <ross.neyman@gmail.com>
2024-04-17 14:35:57 -0400

    Implemented readData for reading in employee data from txt file

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,29 @@
 public class Employee extends Person {
+    private int leadershipRating;
+    private int collaborationRating;
+    private int codingSpeed;
+    private int codingDesign;
+    private String preferredRole;
 
+    public Employee(){
+        name = null;
+        leadershipRating = 0;
+        collaborationRating = 0;
+        codingSpeed = 0;
+        codingDesign = 0;
+        preferredRole = null;
+    }
+    public Employee(String n, int l, int col, int codS, int codD, String p) {
+        name = n;
+        leadershipRating = l;
+        collaborationRating = col;
+        codingSpeed = codS;
+        codingDesign = codD;
+        preferredRole = p;
+    }
 
-    public Employee(){name = null;}
-    public Employee(String n){name = n;}
+    public int getLeadershipRating(){return leadershipRating;}
+    public int getCollaborationRating() {return collaborationRating;}
+    public int getCodingDesign() {return codingDesign;}
+    public int getCodingSpeed() {return codingSpeed;}
 }
