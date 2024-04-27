2886f9bb90e806440283228db4694c94ba27c23d
RossNeyman <ross.neyman@gmail.com>
2024-04-27 10:47:57 -0400

    create manager class

--- src/Sorting.java
+++ src/Sorting.java
@@ -4,25 +4,8 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class Sorting {
-    private ArrayList<Employee> employeeArrayList = new ArrayList<Employee>();
-    //Uses ArrayList for speed in sorting algorithms being more of a priority than memory management
 
-    public void readData(File f) throws FileNotFoundException {
-        Scanner myReader = new Scanner(f);
-        while (myReader.hasNextLine()) {
-            employeeArrayList.add(new Employee(
-                    myReader.nextLine(), //name
-                    myReader.nextInt(),  //ID
-                    myReader.nextInt(),  //leadershipRating
-                    myReader.nextInt(),  //collaborationRating
-                    myReader.nextInt(),  //codingSpeed
-                    myReader.nextInt(),  //codingDesign
-                    myReader.nextLine()  //preferredRole
-            ));
-        }
-    }
-    public void writeData(File f) throws FileNotFoundException {
 
-    }
+
 
 }
