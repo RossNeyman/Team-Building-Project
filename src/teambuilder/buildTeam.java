39454ab6cc6f943b27c4d17fb40c7b624d2ecf70
nikolasHun <radnik2004@gmail.com>
2024-04-29 11:56:37 -0400

    Creation of the buildTeam class.

---
+++ src/teambuilder/buildTeam.java
@@ -0,0 +1,32 @@
+package teamBuilder
+
+public class buildTeam{
+    private ArrayList<Employees> TeamList = new ArrayList<Employees>();
+    private int memberCount;
+    private int maxMembers;
+
+    /**
+     * Adds members to the team.
+     * To add a member the the memberCount must be less than maxMembers.
+     */
+    public void addMember(){
+
+    }
+
+
+    /**
+     * Remove member from the team.
+     * The memeber must exist in th team, in order to be removed.
+     */
+    public void removeMember(){
+
+    }
+
+    /**
+     * Display members of the team.
+     */
+    public void viewMembers(){
+
+    }
+
+    }
