0822a89a3eb07ab2384740e5f78820ef436dacb9
nikolasHun <radnik2004@gmail.com>
2024-07-09 14:27:17 -0400

    Two parameterized constructors added, both with provide class with support from employeeDB. One allows for priority and max members to be set. The constructors work. However, most methods in class have yet to be tested.

--- src/teambuilder/suggestionAlgorithm.java
+++ src/teambuilder/suggestionAlgorithm.java
@@ -1,26 +1,57 @@
 package teambuilder;
+import teambuilder.util.EmployeeDBTools;
+
 import java.util.ArrayList;
 import java.util.Map;
 
 public class suggestionAlgorithm{
     private final int attributeNum = 4;
     private final int maxScoreIndividual = attributeNum * 10;
-    private int teamCountMax; // value should be set in a constructor.
+    private final double minPercent = 0.7;
+    private int teamCountMax; // has a default value;
     private final int maxScore = maxScoreIndividual * teamCountMax; // the calculation should be done in a constructor.
     private int memberCount = 0; // The following operations of this class should not be performed if the teamCount is less than 1.
     private int desiredTeamScore; // based on the entered desired team member count.
     private int currentTeamScore; // based on the current number of employees
     private int currentScore;
     private int minScore;
-    private int priority; // min value will be 0 and max 4.  Ranging from no priority to speed, design, collaboration, and leadership.
+    private int priority = 'N'; // min value will be 0 and max 4.  Ranging from no priority to speed, design, collaboration, and leadership. // ignore
     private Map<Integer, Integer> mappedScores; // key is an employee score, and value is the count of employees with said score.
 
     private char additionPriority; // stores the suggesting priority, value should be entered in constructor.
-    // selection is as follows: T for team, S for speed, D for design, C for collab, and L for leadership.
+    // selection is as follows: T for team, S for speed, D for design, C for collab, and L for leadership. Also default value will be N for none.
+    private EmployeeDBTools dataB; // provides access to the database.
 
+    /**
+     * parameterized constructor supports database access, priority selection, and max member change.
+     * @param db
+     * @param aPriority
+     * @param maxMembers
+     */
+    public suggestionAlgorithm(EmployeeDBTools db, char aPriority, int maxMembers){
+        dataB = db;
+        additionPriority = aPriority;
+        teamCountMax = maxMembers;
+        currentTeamScore = 0;
+    }
+
+    /**
+     * parameterized constructor provides db support.
+     * @param db
+     */
+    public suggestionAlgorithm(EmployeeDBTools db){
+        dataB = db;
+        teamCountMax = 4;
+        currentTeamScore = 0;
+    }
+
+    /**
+     * Sets the max value for the team.
+     * @param max
+     */
     public void setTeamMax(int max){
-        this.teamCountMax = max;
-    } // may not need this, when creating constructor.
+        teamCountMax = max;
+    }
 
     /**
      * Calculates the minimum acceptable score for a team.
@@ -29,7 +60,7 @@ public class suggestionAlgorithm{
      */
     public double minReqScoreCalc() throws Exception{
         if(memberCount > 0){
-            return (double)maxScore * 0.7;
+            return (double)maxScore * minPercent;
         }
         else throw new Exception("Team is empty\n");
     }
@@ -44,7 +75,7 @@ public class suggestionAlgorithm{
 
     /**
      * Updates the teams current score.
-     * @param value
+     * @param value value passed should be an employees overall score.
      */
     public void updateScore(int value){
         currentScore += value; // maybe the realtime updates should be handled by the teamBuilder class.
@@ -71,5 +102,6 @@ public class suggestionAlgorithm{
             }
         }
     }
+    //TODO need access to employee database for search and retrevial operations, also consider using this for mapping.
 
 }
